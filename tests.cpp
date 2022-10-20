9e4fa62d1eb1b0b403d9aa50fe13a08cd581dfe8
Denis Vorkozhokov <30625073+lejabque@users.noreply.github.com>
2022-10-20 14:46:29 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,10 +1,19 @@
 #include "gtest/gtest.h"
+#include <stdexcept>
 
-int sum(int a, int b) {
+static int sum(int a, int b) {
   // In normal tasks, never change files with tests but here you can :)
   return a - b; // TODO: fix me
 }
 
 TEST(something, simple) {
   ASSERT_EQ(2, sum(1, 1));
+}
+
+static void throwing_func() {
+  throw std::logic_error("some exception");
+}
+
+TEST(something, test_abi) {
+  EXPECT_THROW(throwing_func(), std::logic_error);
 }
