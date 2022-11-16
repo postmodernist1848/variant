a37e71fd11bce6598428a8ecb05e448bdcd2517f
Denis Vorkozhokov <30625073+lejabque@users.noreply.github.com>
2022-11-17 01:30:38 +0300

    bump ubuntu to 22, ignore alloc-dealloc mismatch for libcxx

--- tests.cpp
+++ tests.cpp
@@ -1,10 +1,6 @@
 #include "gtest/gtest.h"
 #include <stdexcept>
 
-TEST(something, simple) {
-  ASSERT_EQ(2, sum(1, 1));
-}
-
 static void throwing_func() {
   throw std::logic_error("some exception");
 }
