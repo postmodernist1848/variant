cbbc628f049effb8f8238ffcfe3eec67fab365a6
Denis Vorkozhokov <30625073+lejabque@users.noreply.github.com>
2022-11-17 01:05:42 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,6 @@
 #include "gtest/gtest.h"
 #include <stdexcept>
 
-static int sum(int a, int b) {
-  // In normal tasks, never change files with tests but here you can :)
-  return a - b; // TODO: fix me
-}
-
 TEST(something, simple) {
   ASSERT_EQ(2, sum(1, 1));
 }
