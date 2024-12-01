3ac86dd2659dfdb0b4bc76ec82503f7b6c81178f
Vladimir Ivanov <elitic.pantheism@gmail.com>
2024-12-01 03:45:10 +0300

    refactor visit

--- src/visit.h
+++ src/visit.h
@@ -11,7 +11,7 @@ template <typename T, std::size_t Size, std::size_t... Sizes>
 struct table {
   table<T, Sizes...> arr[Size];
 
-  // Construct is a class with a static template member 'value' of type T
+  // Constructor is a class with a static template member 'value' of type T
   template <typename Constructor, std::size_t... PrevIs, std::size_t... Is>
   constexpr table(Constructor c, std::index_sequence<PrevIs...>, std::index_sequence<Is...>)
       : arr{table<T, Sizes...>(c, std::index_sequence<PrevIs..., Is>{})...} {}
@@ -43,11 +43,11 @@ struct table<T, Size> {
   }
 };
 
-template <typename R, typename Visitor, typename... Storages>
+template <typename R, typename Visitor, typename... Variants>
 struct storage_visit_constructor {
   template <std::size_t... Is>
-  static constexpr auto value = +[](Visitor&& vis, Storages&&... storages) -> R {
-    return std::forward<Visitor>(vis)(storage::constant::get<Is>(std::forward<Storages>(storages))...);
+  static constexpr auto value = +[](Visitor&& vis, Variants&&... vars) -> R {
+    return std::forward<Visitor>(vis)(get<Is>(std::forward<Variants>(vars))...);
   };
 };
 
@@ -57,21 +57,16 @@ constexpr R visit_impl(Visitor&& vis, Variants&&... vars) {
   if ((vars.valueless_by_exception() || ...)) {
     throw bad_variant_access{};
   }
-  using table = table<
-      R (*const)(Visitor&&, decltype((std::declval<Variants>()._storage))...),
-      variant_size_v<std::remove_cvref_t<Variants>>...>;
-  using Constructor = storage_visit_constructor<R, Visitor, decltype((std::declval<Variants>()._storage))...>;
-  constexpr table tbl(Constructor{});
-  return tbl(vars.index()...)(std::forward<Visitor>(vis), std::forward<Variants>(vars)._storage...);
+  using table = table<R (*const)(Visitor&&, Variants&&...), variant_size_v<std::remove_cvref_t<Variants>>...>;
+  constexpr table tbl(storage_visit_constructor<R, Visitor, Variants&&...>{});
+  return tbl(vars.index()...)(std::forward<Visitor>(vis), std::forward<Variants>(vars)...);
 }
 
 } // namespace variant_detail
 
 template <typename Visitor, typename... Variants>
 constexpr decltype(auto) visit(Visitor&& vis, Variants&&... vars) {
-  using R = decltype(std::forward<Visitor>(vis)(
-      variant_detail::storage::constant::get<0>(std::forward<Variants>(vars)._storage)...
-  ));
+  using R = decltype(std::forward<Visitor>(vis)(get<0>(std::forward<Variants>(vars))...));
   return variant_detail::visit_impl<R>(std::forward<Visitor>(vis), std::forward<Variants>(vars)...);
 }
 
