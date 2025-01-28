046ba32f67a93aab1ec2a9798d6fe1984f73f556
Vladimir Ivanov <elitic.pantheism@gmail.com>
2025-01-28 03:02:15 +0300

    apply clang-format

--- src/variant.h
+++ src/variant.h
@@ -131,8 +131,7 @@ public:
     requires (!std::same_as<std::remove_cvref_t<T>, variant>) && (!variant_detail::is_in_place_index<T>::value) &&
              (!variant_detail::is_in_place_type<T>::value) &&
              std::is_constructible_v<variant_detail::non_narrowing_overload_t<T, Types...>, T>
-  constexpr variant(
-      T&& t
+  constexpr variant(T&& t
   ) noexcept(std::is_nothrow_constructible_v<variant_detail::non_narrowing_overload_t<T, Types...>, T>)
       : variant(in_place_type<variant_detail::non_narrowing_overload_t<T, Types...>>, std::forward<T>(t)) {}
 
@@ -274,9 +273,8 @@ public:
     lhs.swap(rhs);
   }
 
-  constexpr void swap(variant& other) noexcept(
-      ((std::is_nothrow_move_constructible_v<Types> && std::is_nothrow_swappable_v<Types>) && ...)
-  ) {
+  constexpr void swap(variant& other
+  ) noexcept(((std::is_nothrow_move_constructible_v<Types> && std::is_nothrow_swappable_v<Types>) && ...)) {
     if (this->valueless_by_exception() && other.valueless_by_exception()) {
       return;
     }
@@ -350,8 +348,8 @@ constexpr std::add_pointer_t<variant_alternative_t<I, variant<Types...>>> get_if
 }
 
 template <std::size_t I, typename... Types>
-constexpr std::add_pointer_t<const variant_alternative_t<I, variant<Types...>>>
-get_if(const variant<Types...>* pv) noexcept {
+constexpr std::add_pointer_t<const variant_alternative_t<I, variant<Types...>>> get_if(const variant<Types...>* pv
+) noexcept {
   if (pv && I == pv->index()) {
     return std::addressof(get<I>(*pv));
   } else {
